30dc7bd12e9132418e1ed4183ef79be1e72d594c
Marco Alves <marco.m.alves@gmail.com>
2013-10-04 14:36:41 +0100

    Initial Commit

---
+++ ScrollViewInsideTableViewCellExperiment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ScrollViewInsideTableViewCellExperiment
+//
+//  Created by Marco Alves on 04/10/13.
+//  Copyright (c) 2013 Marco Alves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ ScrollViewInsideTableViewCellExperimentTests/ScrollViewInsideTableViewCellExperimentTests.m
@@ -0,0 +1,34 @@
+//
+//  ScrollViewInsideTableViewCellExperimentTests.m
+//  ScrollViewInsideTableViewCellExperimentTests
+//
+//  Created by Marco Alves on 04/10/13.
+//  Copyright (c) 2013 Marco Alves. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScrollViewInsideTableViewCellExperimentTests : XCTestCase
+
+@end
+
+@implementation ScrollViewInsideTableViewCellExperimentTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
